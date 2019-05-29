b12e9e8035dc35428373f234fa90c8f8b1e35cd3
aaronwht <awhite@tarvold.com>
2019-05-28 19:06:36 -0700

    Updated Version

--- index.js
+++ index.js
@@ -4,18 +4,18 @@
 //require('dotenv').config()
 
 module.exports.handler = async (event, context) => {
-    return {
-      statusCode: 200,
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({v: 1, status:'success'})
-    }
-
-    /*
-    callback(null, {
-        "statusCode": 200,
-        "body": {
-            'message': 'here ya go'
-        }
-    })
-    */
+  return {
+    statusCode: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ v: 2, status: 'success' })
   }
+
+  /*
+  callback(null, {
+      "statusCode": 200,
+      "body": {
+          'message': 'here ya go'
+      }
+  })
+  */
+}
