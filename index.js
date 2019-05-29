eec2132e29b20599a865550da2f26e83d3195f5a
aaronwht <awhite@tarvold.com>
2019-05-28 19:15:37 -0700

    Added notes model and connected database

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 //'use strict';
 //var moment = require('moment');
-const connectToDatabase = require('./db');
+const connectToDatabase = require('./db')
+const Note = require('./Note')
 require('dotenv').config()
 
 module.exports.handler = async (event, context) => {
+  context.callbackWaitsForEmptyEventLoop = false
+  await connectToDatabase(event)
+  const notes = await Note.find()
   return {
     statusCode: 200,
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ v: 2, status: 'success' })
+    body: JSON.stringify({ v: 3, notes, status: 'success' })
   }
 
   /*
